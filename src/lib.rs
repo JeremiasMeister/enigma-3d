e24d68ab01ddd5c23e6e6dc900a1a8d2abb3a92b
Jeremias Meister <contact@cg-jm.com>
2024-08-11 20:43:50 +0200

    did hide resources module behind feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,8 @@ pub mod postprocessing;
 pub mod ui;
 pub mod resources;
 pub mod data;
+
+#[cfg(not(feature = "publish"))]
 pub mod example_resources;
 
 pub fn init_default(app_state: &mut AppState) {
